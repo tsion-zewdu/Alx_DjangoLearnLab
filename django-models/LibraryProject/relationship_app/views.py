7282f9207254ab4f6d694dc5391f18e7fd27fca9
tsion <tsion@gmail.com>
2026-01-30 10:04:38 -0800

    Fix permission_required decorators and secured book URLs

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -72,17 +72,22 @@ def librarian_view(request):
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
-@permission_required('relationship_app.canaddbook', raise_exception=True)
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
         title = request.POST.get('title')
+        publication_year = request.POST.get('publication_year')
         author_id = request.POST.get('author')
-        year = request.POST.get('publication_year')
-        Book.objects.create(title=title, author_id=author_id, publication_year=year)
+        Book.objects.create(
+            title=title,
+            publication_year=publication_year,
+            author_id=author_id
+        )
         return redirect('list_books')
     return render(request, 'relationship_app/add_book.html')
 
-@permission_required('relationship_app.canchangebook', raise_exception=True)
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
@@ -92,7 +97,8 @@ def edit_book(request, book_id):
         return redirect('list_books')
     return render(request, 'relationship_app/edit_book.html', {'book': book})
 
-@permission_required('relationship_app.candeletebook', raise_exception=True)
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
