5a2e8cb5f38a7432cf39a49b5279ac7b45f83fd9
tsion <tsion@gmail.com>
2026-01-30 10:08:10 -0800

    fix path

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path('admin-role/', admin_view, name='admin_view'),
     path('librarian-role/', librarian_view, name='librarian_view'),
     path('member-role/', member_view, name='member_view'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
 
