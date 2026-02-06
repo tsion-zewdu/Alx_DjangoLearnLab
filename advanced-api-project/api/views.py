c45e06850666640c60191abd6cf67fc9755ce1c0
tsion <tsion@gmail.com>
2026-02-06 13:32:02 -0800

    Fix filters.SearchFilter and filters.OrderingFilter for ALX compliance

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.filters import SearchFilter  # keep SearchFilter
-from django_filters import rest_framework as filters  # required for ALX OrderingFilter
+from django_filters import rest_framework as filters  
 
 from .models import Book
 from .serializers import BookSerializer
+
 
 # READ – anyone can access, but authenticated can also write if needed
 class BookListView(generics.ListAPIView):
@@ -37,8 +37,7 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    # DRF backends (ALX expects filters.OrderingFilter here)
-    filter_backends = [filters.DjangoFilterBackend, SearchFilter, filters.OrderingFilter]
+    filter_backends = [filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
 
     # Fields available for filtering
     filterset_fields = ['title', 'author', 'publication_year']
