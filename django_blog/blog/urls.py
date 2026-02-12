a19675080f57ec796b54d93da52411198b6b7f66
tsion <tsion@gmail.com>
2026-02-12 13:23:47 -0800

    Add structured URLs

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,8 +25,10 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
 ]
+
+
